f7747eb13c113c2e85e119b0c7f27b931e224ee7
aaron <afarrell002@gmail.com>
2022-01-05 19:13:20 -0700

    added landingpage routes to controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
+const landingpageRoutes = require("./landingpage-routes");
 
 router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
+router.use("/landingpage", landingpageRoutes);
 
 module.exports = router;
