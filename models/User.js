7b696856cd264a5412560ebc0b4aef3e879117f2
aaron <afarrell002@gmail.com>
2022-01-05 21:00:45 -0700

    all posts admin handlebars

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("../config/connection");
+
+class User extends Model {
+  passwordAuth(userPassword) {
+    return bcrypt.compareSync(userPassword, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6],
+        notEmpty: true,
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
