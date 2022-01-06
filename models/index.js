77f0adae5643e697533e239535584d1a1f5d64cf
aaron <afarrell002@gmail.com>
2022-01-05 22:58:10 -0700

    updated models/index.js to inclue the associations between the db tables, and added freezetablename true to user model

--- models/index.js
+++ models/index.js
@@ -1,56 +1,24 @@
-const { Model, DataTypes } = require("sequelize");
-const bcrypt = require("bcrypt");
-const sequelize = require("../config/connection");
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
 
-class User extends Model {
-  passAuth(userPassword) {
-    return bcrypt.compareSync(userPassword, this.password);
-  }
-}
+Post.belongsTo(User, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
 
-User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [6],
-        notEmpty: true,
-      },
-    },
-  },
-  {
-    hooks: {
-      beforeCreate: async (userSignupData) => {
-        userSignupData.password = await bcrypt.hash(
-          userSignupData.password,
-          10
-        );
-        return userSignupData;
-      },
-      beforeUpdate: async (userUpdatedData) => {
-        userUpdatedData.password = await bcrypt.hash(
-          userUpdatedData.password,
-          10
-        );
-        return userUpdatedData;
-      },
-    },
-    sequelize,
-    freezeTableName: true,
-    modelName: "user",
-  }
-);
+Post.hasMany(Comment, {
+  foreignKey: "postId",
+  onDelete: "CASCADE",
+});
 
-module.exports = User;
+Comment.belongsToMany(User, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+
+module.exports = {
+  User,
+  Comment,
+  Post,
+};
